427809554bfd54a0b78b6fa1559bfa9ad7503a33
MARCELO SILVA DE PAULA FILHO <0200832311028>
2024-09-24 09:47:16 -0300

    arquivos front-end

--- app.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('button').addEventListener('click', function() {
-    alert('Fetching latest cucumber data...');
-});
---
+++ script.js
@@ -0,0 +1,4 @@
+function atualizarDados() {
+    document.getElementById('qualidadeAgua').textContent = "7.5 ppm";
+    document.getElementById('nivelIrrigacao').textContent = "Nível 3";
+}
